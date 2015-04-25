104e4af061fd7063ee68e8d127ced1d2fcd99462
Osman Emre <osmanemreercin@gmail.com>
2015-04-25 15:50:33 +0300

    Create / View Poll function is added.

--- SocioActiveClient/js/config.js
+++ SocioActiveClient/js/config.js
@@ -22,6 +22,41 @@ function config($stateProvider, $urlRouterProvider, $ocLazyLoadProvider) {
             url: "/index",
             templateUrl: "views/common/content.html"
         })
+        .state('profile', {
+            abstract: true,
+            url: "/profile",
+            templateUrl: "views/common/content.html"
+        })
+        .state('profile.user', {
+            url: "/profile/user",
+            templateUrl: "views/profile.html",
+            data: { pageTitle: 'Profile Page' },
+            resolve: {
+                loadPlugin: function ($ocLazyLoad) {
+                    return $ocLazyLoad.load([
+                        {
+                            name: 'datePicker',
+                            files: ['css/plugins/datapicker/angular-datapicker.css','js/plugins/datapicker/datePicker.js']
+                        },
+                        {
+                            files: ['js/plugins/jasny/jasny-bootstrap.min.js']
+                        },
+                        {
+                            name: 'ui.tree',
+                            files: ['css/plugins/uiTree/angular-ui-tree.min.css','js/plugins/uiTree/angular-ui-tree.min.js']
+                        },
+                        {
+                            name: 'ngTagsInput',
+                            files: ['css/plugins/ngTags/ng-tags-input.css','js/plugins/ngTags/ng-tags-input.js']
+                        },
+                        {
+                            name: 'ui.select',
+                            files: ['css/plugins/ui-select/select.css','js/plugins/ui-select/select.js']
+                        }
+                    ]);
+                }
+            }
+        })
         .state('index.main', {
             url: "/main",
             templateUrl: "views/main.html",
