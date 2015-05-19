8d6defb5859126710f7dfa27d593e7c3cf3a9fff
Övünç <ovuncuzun@sabanciuniv.edu>
2015-05-19 21:06:34 +0300

    event updated

--- SocioActiveClient/js/config.js
+++ SocioActiveClient/js/config.js
@@ -161,6 +161,37 @@ function config($stateProvider, $urlRouterProvider, $ocLazyLoadProvider) {
                 }
             }
         })
+        .state('activity.events2', {
+            url: "/view_events",
+            templateUrl: "views/event_view_new.html",
+            controller: 'EventCtrl',
+            data: { pageTitle: 'View Events'},
+            resolve: {
+                loadPlugin: function ($ocLazyLoad) {
+                    return $ocLazyLoad.load([
+                        {
+                            name: 'datePicker',
+                            files: ['css/plugins/datapicker/angular-datapicker.css','js/plugins/datapicker/datePicker.js']
+                        },
+                        {
+                            files: ['js/plugins/jasny/jasny-bootstrap.min.js']
+                        },
+                        {
+                            name: 'ui.tree',
+                            files: ['css/plugins/uiTree/angular-ui-tree.min.css','js/plugins/uiTree/angular-ui-tree.min.js']
+                        },
+                        {
+                            name: 'ngTagsInput',
+                            files: ['css/plugins/ngTags/ng-tags-input.css','js/plugins/ngTags/ng-tags-input.js']
+                        },
+                        {
+                            name: 'ui.select',
+                            files: ['css/plugins/ui-select/select.css','js/plugins/ui-select/select.js']
+                        }
+                    ]);
+                }
+            }
+        })
         .state('activity.instantaneous', {
             url: "/email_template",
             templateUrl: "views/main.html",
