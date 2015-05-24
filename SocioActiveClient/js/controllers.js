422fc04e8460a242d925678080995d935dc0f89b
tugbademiryurek <tugbademiryurek.1990@gmail.com>
2015-05-24 15:53:37 +0300

    fixes

--- SocioActiveClient/js/controllers.js
+++ SocioActiveClient/js/controllers.js
@@ -879,6 +879,8 @@ function ProfileCtrl($scope, $rootScope, fireFactory) {
         });
     };
 
+    $scope.alert = function(x){window.alert("My interest is "+x);};
+    $scope.showDetail = false;
 
     $scope.getClass = function (item) {
         if (!item.userImageSmall) {
@@ -907,11 +909,16 @@ function ProfileViewCtrl($scope, $rootScope, fireFactory, $stateParams) {
             $scope.specificrole = loadedData.role;
             $scope.specificUserName = loadedData.userName;
             $scope.specificUserLastName = loadedData.userLastName;
+            $scope.specificdescription = loadedData.description;
+            $scope.specificcontexts = loadedData.contexts;
             $scope.specificUserImage = loadedData.userImage;
 
         });
 
     };
+
+    $scope.alert = function(x){window.alert($scope.specificUserName+"'s interest is "+x);};
+    $scope.showDetail = false;
 
     $scope.getClass = function (item) {
         if (!item.userImageSmall) {
